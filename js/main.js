d9fccfe4f2e8078336c40dc8b27f34417e5f2599
samzah <szahir@hotmail.com>
2016-04-01 16:42:27 -0400

    making circle move

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var $box = $('.box');
+var $diamond = $('.diamond');
+var $panel = $('.panel');
+var $circle = $('circle');
+var $list = $('list');
+
+var $btnShowHide = $('.btn-show-hide');
+var $btnMove = $ ('.btn-move');
+var $btnCollapseexpand = $ ('.btn-collapse-expand');
+var $btnBounce = $ ('.btn-bounce');
+
+$btnShowHide.on('click', function (e) {
+  $box.toggleClass('is-clicked');
+});
+
+$btnMove.on('click', function (e) {
+  $diamond.toggleClass('diamond-click');
+});
+
+$btnCollapseexpand.on('click', function (e) {
+  $panel.toggleClass('panel-click');
+});
+
+$btnBounce.on('click', function (e) {
+  $circle.toggleClass('circle-click');
+});
